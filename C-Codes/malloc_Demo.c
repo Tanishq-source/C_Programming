624436eee9ba8adcfefb950cb43b355666df7875
Tanishq chandole <tanishqchandole@gmail.com>
2026-02-14 23:21:19 +0530

    Codes

---
+++ C-Codes/malloc_Demo.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int length = 0;
+    int *Arr = NULL;
+
+    printf("Enter the number of elements :\n");
+    scanf("%d",&length);
+
+    // step 1 : allocate the memory
+    Arr = (int *)malloc(length * sizeof(int));
+    if
+    (Arr == NULL)
+    {
+        printf("Unable to allocate memory\n");
+    }
+    else
+    {
+        printf("Memory gets succesfully allocated\n");
+    }
+
+    //step 2 :use the memory 
+
+    //step 3 :Free the memory
+    free(Arr);
+
+
+
+
+    return 0;
+}
