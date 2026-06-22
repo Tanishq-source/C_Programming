ae4d6c21aeef419d415fa5c967534aaff8965df5
Tanishq chandole <tanishqchandole@gmail.com>
2026-06-22 13:12:28 +0530

    Codes

---
+++ C-Codes/program230.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void CountAll(char str[])
+{
+   int iCountSmall = 0;
+   int iCountCapital = 0;
+
+   while(*str != '\0')
+      {
+         if((*str >= 'A') && (*str <= 'Z'))
+         {
+            iCountCapital++;
+         }
+         else
+         {
+            iCountSmall++;
+         }
+         str++;
+      }
+
+      printf("Number of small Characters : %d\n",iCountSmall);
+      printf("Number of Capital Characters : %d\n",iCountCapital);
+      
+      
+     
+}
+
+int main()
+{
+   char Arr[50] = {'\0'};
+   
+   
+
+   printf("Enter String : \n");
+   scanf("%[^'\n']s",Arr);
+
+   CountAll(Arr);
+
+   return 0;
+}
