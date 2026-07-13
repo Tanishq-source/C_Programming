53f486e99a0825447079d325db15c1ad506ecd68
Tanishq chandole <tanishqchandole@gmail.com>
2026-07-13 19:56:19 +0530

    Codes

---
+++ C-Codes/program269.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+float Summation(float Arr[],int iSize)
+{
+    int iCnt = 0;
+    float iSum = 0.0f;
+
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+    return iSum;
+}
+
+int main()
+{
+    float Brr[] = {10.1f,20.2f,30.3f,40.4f,50.5f};
+    float fRet = 0.0f;
+
+    fRet = Summation(Brr,5);
+
+    cout<<"Summation is :"<<fRet<<"\n";
+
+    return 0;
+}
